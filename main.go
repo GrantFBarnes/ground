5f4605a31eeacd502d1528b02f1a2a65bdc592fd
Grant Barnes <gfbarnes78@gmail.com>
2025-07-06 09:25:34 -0500

    setup basic web server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("ground")
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello")
+	})
+
+	port := ":3478"
+	log.Printf("server is running on http://localhost%s...\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
