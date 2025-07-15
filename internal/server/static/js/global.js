21e9670d7830340a3a55e2974822e8486aa80f44
Grant Barnes <gfbarnes78@gmail.com>
2025-07-15 12:24:01 -0500

    remove common call Post API method

--- internal/server/static/js/global.js
+++ internal/server/static/js/global.js
@@ -1,19 +1,3 @@
-function logout() {
-    if (confirm("Are you sure you want to logout?")) {
-        callPostAPI("/api/logout", null).then(() => location.reload());
-    }
-}
-
-async function callPostAPI(url, body) {
-    try {
-        const response = await fetch(url, { method: "POST", body: body });
-        const text = await response.text();
-        return { status: response.status, data: text };
-    } catch (error) {
-        return { status: "error", data: error };
-    }
-}
-
 function toggleLoader() {
     const loaderElement = document.getElementById("loader");
     if (loaderElement) {
@@ -23,5 +7,11 @@ function toggleLoader() {
         newLoaderElement.id = "loader";
         newLoaderElement.className = "loader";
         document.body.appendChild(newLoaderElement);
+    }
+}
+
+function logout() {
+    if (confirm("Are you sure you want to logout?")) {
+        fetch("/api/logout", { method: "POST" }).then(() => location.reload());
     }
 }
