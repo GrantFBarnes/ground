ed7babf0c6316734d7491471290d0770c5f1eeec
Grant Barnes <gfbarnes78@gmail.com>
2025-07-15 14:40:48 -0500

    add overlay to loading, add notification element

--- internal/server/static/js/global.js
+++ internal/server/static/js/global.js
@@ -1,13 +1,32 @@
-function toggleLoader() {
-    const loaderElement = document.getElementById("loader");
-    if (loaderElement) {
-        loaderElement.remove();
+function toggleLoading() {
+    let overlayElement = document.getElementById("overlay");
+    let loadingWheelElement = document.getElementById("loading-wheel");
+
+    if (overlayElement || loadingWheelElement) {
+        overlayElement.remove();
+        loadingWheelElement.remove();
     } else {
-        const newLoaderElement = document.createElement("div");
-        newLoaderElement.id = "loader";
-        newLoaderElement.className = "loader";
-        document.body.appendChild(newLoaderElement);
+        overlayElement = document.createElement("div");
+        overlayElement.id = "overlay";
+        document.body.appendChild(overlayElement);
+
+        loadingWheelElement = document.createElement("div");
+        loadingWheelElement.id = "loading-wheel";
+        document.body.appendChild(loadingWheelElement);
     }
+}
+
+function displayNotification(message) {
+    let notificationElement = document.getElementById("notification");
+    if (notificationElement) {
+        notificationElement.remove();
+    }
+
+    notificationElement = document.createElement("div");
+    notificationElement.id = "notification";
+    notificationElement.innerText = message;
+    document.body.appendChild(notificationElement);
+    setTimeout(() => notificationElement.remove(), 5000);
 }
 
 function logout() {
