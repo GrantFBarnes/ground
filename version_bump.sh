c7311ac72bdc06ec65da554f1ae655011510e5b7
Grant Barnes <gfbarnes78@gmail.com>
2025-12-14 15:42:53 -0600

    add snap

--- version_bump.sh
+++ version_bump.sh
@@ -71,6 +71,7 @@ NEW_VERSION="$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
 # update versions
 
 sed -i -E 's/^const VERSION string = "v'$REGEX_VALID_VERSION'"$/const VERSION string = "v'$NEW_VERSION'"/' main.go
+sed -i -E 's/^version: "'$REGEX_VALID_VERSION'"$/version: "'$NEW_VERSION'"/' snap/snapcraft.yaml
 
 ################################################################################
 # print final messages
