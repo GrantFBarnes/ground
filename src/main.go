fe872c9b955774e44b5b1311020c0f3b30e154e7
Grant Barnes <gfbarnes78@gmail.com>
2026-01-18 14:13:15 -0600

    v0.7.0

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/grantfbarnes/ground/internal/system/users"
 )
 
-const VERSION string = "v0.6.3"
+const VERSION string = "v0.7.0"
 
 const COLOR_RED string = "\x1b[31m"
 const COLOR_GREEN string = "\x1b[32m"
